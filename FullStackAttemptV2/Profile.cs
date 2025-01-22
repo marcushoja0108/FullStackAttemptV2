94fe4c02c16f7c91709fceab77e563f1b29ec425
marcushoja0108 <marcushj@live.no>
2025-01-22 08:48:51 +0100

    Basic setup

---
+++ FullStackAttemptV2/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace FullStackAttemptV2;
+
+public class Profile
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Country { get; set; }
+}
---
+++ FullStackAttemptV2/Program.cs
@@ -0,0 +1,47 @@
+using Dapper;
+using FullStackAttemptV2;
+using Microsoft.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://127.0.0.1:5500")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+var connStr = "Data Source=DESKTOP-PSDS24G\\SQLEXPRESS;Initial Catalog=ProfilesTest;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
+
+app.MapGet("/profiles", async () =>
+{
+    var sql = "SELECT * FROM Profiles";
+    var conn = new SqlConnection(connStr);
+    var profiles = await conn.QueryAsync<Profile>(sql);
+    return profiles;
+});
+
+app.MapPost("profiles", async (Profile newProfile) =>
+{
+    var sql = "INSERT INTO Profiles (Name, Age, Country) VALUES (@Name, @Age, @Country)";
+    var conn = new SqlConnection(connStr);
+    var rowsAffected = await conn.ExecuteAsync(sql, new {newProfile.Name, newProfile.Age, newProfile.Country });
+    return rowsAffected;
+});
+
+app.MapDelete("profiles/{id}", async (int id) =>
+{
+    var sql = "DELETE FROM Profiles WHERE Id = @Id";
+    var conn = new SqlConnection(connStr);
+    var rowsAffected = await conn.ExecuteAsync(sql, new { Id = id });
+    return Results.Ok(rowsAffected);
+});
+
+app.Run();
